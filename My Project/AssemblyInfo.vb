b39cf60b93f1ad8b56b9d8d81279d2e3703666e3
geoff <geoff@gdphotog.com>
2020-06-25 11:16:19 +0100

    Increased app level

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -9,7 +9,7 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("Covid19-Itinerary")>
-<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyDescription("Cold Shutdown Itinerary generator for IQ Photo system on RCCL ships")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("Covid19-Itinerary")>
 <Assembly: AssemblyCopyright("Copyright ©  2020")>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.0.0.2")>
+<Assembly: AssemblyFileVersion("1.0.0.2")>
