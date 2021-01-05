f09044a19a71f4a224ed28b93bad1a80a2ffd257
geoff <geoff@gdphotog.com>
2021-01-05 11:38:59 +0000

    Added Manifest creation

--- Form1.vb
+++ Form1.vb
@@ -1,8 +1,5 @@
-﻿Imports System
-Imports System.Text
+﻿Imports System.Text
 Imports System.IO
-Imports System.Linq
-Imports System.Data
 Imports System.Deployment.Application
 
 
@@ -22,20 +19,49 @@ Public Class Form1
     Public StaffName As String = "COVID 19, Test User"
     Public Staffid As Integer = 10000
     Public cruiseline As String
-    Public DebugON As Boolean = False
+    Public DebugON As Boolean
     Public firstrun As Boolean = True
-
+    Public numberofguests As Integer = 10
+    Public runmode As String
+    Public manifest As Boolean
+    Dim mOnesArray(8) As String
+    Dim mOneTensArray(9) As String
+    Dim mTensArray(7) As String
+    Dim mPlaceValues(4) As String
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Call the reset Date Picker and ShipCode & Days
 
 
         If firstrun = True Then
+            'Read settings from app config file
+            Try
+                DebugON = Convert.ToBoolean(My.Settings.DebugOn)
+                runmode = My.Settings.RunMode.ToUpper
+                If runmode = "SHIP" Then
+                    manifest = False
+                ElseIf runmode = "LAB" Then
+                    manifest = True
+                    Try
+                        numberofguests = Integer.Parse(My.Settings.NumberOfGuests)
+
+                    Catch ex As Exception
+                        MsgBox(ex.Message, MsgBoxStyle.Critical, "Number of Guests set incorrectly in App Config")
+                        End
+                    End Try
+                Else
+                    Throw New Exception("RunMode not set correctly in application Config file")
+                End If
+
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Critical)
+                End
+            End Try
             Setupform()
             firstrun = False
 
         Else
-        ResetForm()
+            ResetForm()
         End If
 
     End Sub
@@ -109,9 +135,10 @@ Public Class Form1
     End Sub
 
     Private Sub Populate_Days()
-        Dim i As Integer
-        For i = 0 To 45
-            CboNumberOfDays.Items.Add(i)
+        Dim populatedays As Integer
+        For populatedays = 0 To 45
+
+            CboNumberOfDays.Items.Add(populatedays)
         Next
         CboNumberOfDays.Text = "0"
     End Sub
@@ -136,6 +163,9 @@ Public Class Form1
         Dim portdate As String
         Dim voyagetovalidate As String
         Dim validatedvoyage As Integer
+        Dim manifestStartdate As String
+        Dim manifestenddate As String
+        Dim dateformat As String
 
         'Validate that Cruise is an integer less than 99999
         voyagetovalidate = txtVoyageNumber.Text
@@ -196,8 +226,10 @@ Public Class Form1
                 MsgBox(portdate)
             End If
             If dayofcruise = 0 Then
+                manifestStartdate = workingdate.ToString("yyyyMMdd")
                 portcode = embarkport
             ElseIf dayofcruise = maxdays Then
+                manifestenddate = workingdate.ToString("yyyyMMdd")
                 portcode = embarkport
             Else
                 portcode = seaday
@@ -246,6 +278,8 @@ Public Class Form1
         ElseIf cruiseline = "CEL" Then
             'Sets the Photog Type to Lab Tech for CEL.
             photogtype = "CPLT"
+        Else
+            photogtype = "RPLT"
         End If
 
         staffdt.Rows.Add(Twocharshipcode, validatedvoyage, Staffid, photogtype, "Mr", StaffName)
@@ -262,6 +296,94 @@ Public Class Form1
         Using writer As StreamWriter = New StreamWriter(staffstreampath)
             Rfc4180Writer.WriteDataTable(staffdt, writer, True)
         End Using
+
+        'Create the Manifest file
+        Dim manifestdt As New DataTable
+
+        manifestdt.Columns.Add("ManifestEntry")
+
+
+        Dim ManifestPort As String = "MIA"
+        Dim ManifestStatus As String = "O"
+        Dim manifestSeq As String = "01"
+        Dim manifestshipcode As String = Twocharshipcode & "S"
+        Dim manifestTower As String = "   "
+        Dim ManifestFolder As String = "   "
+        Dim manifestGuestFirstName As String = "TEST"
+        Dim manifestGuestLastName As String = "GUEST "
+        Dim manifestvoyage As String
+        Dim manifestsex As String = "M"
+        Dim manifestlang As String = "ENG"
+        Dim manifestnationality As String = "USA"
+        Dim manifestcabin As String
+        Dim manifestFolio As String
+        Dim manifestbooking As String
+        Dim GroupID As String = "0    "
+        Dim groups As String = "0000"
+        Dim cabinsection As String = "074"
+        Dim Loyalty As String = "G"
+        Dim diningtable As String = "0  "
+        Dim Diningroom As String = "American Icon - Deck 3   "
+        Dim diningtime As String = "MY TIME "
+        Dim setsail As String
+        Dim guestid As String
+        Dim manifestentry As String
+
+
+
+        For countermanifest = 0 To numberofguests
+            'countermanifest = countermanifest + 1
+            If Len(validatedvoyage) < 5 Then
+                'Add in length padding 0's in the front of the voyage number
+                validatedvoyage.ToString.PadLeft(5, "0")
+
+            End If
+
+            'Create random cabin, BookingID, GuestID, Folionumber
+
+            Dim foliostart As String = "984100022805"
+            Dim foliornd As Integer = RandomNumber(1000, 9999)
+
+
+            manifestcabin = RandomNumber(1000, 13000)
+            manifestbooking = RandomNumber(1000000, 2000000)
+            guestid = RandomNumber(100000000, 999999999)
+            manifestFolio = foliostart & foliornd.ToString
+
+
+            If Len(manifestcabin) < 5 Then
+                manifestcabin.PadRight(5)
+            End If
+
+
+            'Then create Setsail barcode from GuestID and Cabin
+            setsail = guestid & "-" & manifestcabin
+
+            'Create guest name and pad right the string as well.
+            Dim guestnumber As String
+            Dim manifestguesttmplastname As String
+            guestnumber = ConvertNumberToWords(countermanifest)
+            manifestguesttmplastname = manifestGuestLastName & " " & guestnumber
+            manifestGuestFirstName = manifestGuestFirstName.PadRight(15)
+            manifestGuesttmpLastName = manifestGuesttmpLastName.padright(25)
+
+            manifestentry = manifestshipcode & manifestvoyage & manifestStartdate & manifestGuestLastName & manifestGuestFirstName & ManifestStatus & manifestsex & manifestlang & manifestnationality & manifestcabin & manifestFolio & manifestbooking & manifestSeq & GroupID & groups & cabinsection & Loyalty & manifestStartdate & ManifestPort & manifestenddate & ManifestPort & diningtable & Diningroom & diningtime & manifestTower & ManifestFolder & setsail & guestid
+            manifestdt.Rows.Add(manifestentry)
+        Next
+
+        Dim manifestsb As New StringBuilder
+        Dim finalmanifestfile As String = "manifest_" & Twocharshipcode & "_" & validatedvoyage & ".ttx"
+        Dim manifestfullpath As String = Path.Combine(userdesktop, finalStafffile)
+
+        Dim manifeststreampath As String = stafffullpath
+
+        'Writes the Stringbuilder to file as a stream. The CSV file is RFC4180 compliant.
+        'https://tools.ietf.org/html/rfc4180
+
+        Using writer As StreamWriter = New StreamWriter(manifeststreampath)
+            ttxwriter.WriteDataTable(manifestdt, writer, False)
+        End Using
+
 
         MsgBox("Done")
         ResetForm()
@@ -419,6 +541,30 @@ Public Class Form1
             writer.Flush()
         End Sub
 
+
+
+        Private Shared Function QuoteValue(ByVal value As String) As String
+                Return String.Concat("""", value.Replace("""", """"""), """")
+            End Function
+        End Class
+    Public Class ttxwriter
+        Public Shared Sub WriteDataTable(ByVal sourceTable As DataTable,
+            ByVal writer As TextWriter, ByVal includeHeaders As Boolean)
+            If (includeHeaders) Then
+                Dim headerValues As IEnumerable(Of String) = sourceTable.Columns.OfType(Of DataColumn).Select(Function(column) QuoteValue(column.ColumnName))
+
+                writer.WriteLine(String.Join("", headerValues))
+            End If
+
+            Dim items As IEnumerable(Of String) = Nothing
+            For Each row As DataRow In sourceTable.Rows
+                items = row.ItemArray.Select(Function(obj) QuoteValue(If(obj?.ToString(), String.Empty)))
+                writer.WriteLine(String.Join("", items))
+            Next
+
+            writer.Flush()
+        End Sub
+
         Private Shared Function QuoteValue(ByVal value As String) As String
             Return String.Concat("""", value.Replace("""", """"""), """")
         End Function
@@ -451,6 +597,195 @@ Public Class Form1
 
         End If
     End Sub
+
+    Private Function RandomNumber(min As Integer, max As Integer) As Integer
+        Dim random As New Random()
+        Return random.Next(min, max)
+    End Function 'RandomNumber
+
+
+
+    Public Sub New()
+
+            mOnesArray(0) = "one"
+            mOnesArray(1) = "two"
+            mOnesArray(2) = "three"
+            mOnesArray(3) = "four"
+            mOnesArray(4) = "five"
+            mOnesArray(5) = "six"
+            mOnesArray(6) = "seven"
+            mOnesArray(7) = "eight"
+            mOnesArray(8) = "nine"
+
+            mOneTensArray(0) = "ten"
+            mOneTensArray(1) = "eleven"
+            mOneTensArray(2) = "twelve"
+            mOneTensArray(3) = "thirteen"
+            mOneTensArray(4) = "fourteen"
+            mOneTensArray(5) = "fifteen"
+            mOneTensArray(6) = "sixteen"
+            mOneTensArray(7) = "seventeen"
+            mOneTensArray(8) = "eightteen"
+            mOneTensArray(9) = "nineteen"
+
+            mTensArray(0) = "twenty"
+            mTensArray(1) = "thirty"
+            mTensArray(2) = "forty"
+            mTensArray(3) = "fifty"
+            mTensArray(4) = "sixty"
+            mTensArray(5) = "seventy"
+            mTensArray(6) = "eighty"
+            mTensArray(7) = "ninety"
+
+            mPlaceValues(0) = "hundred"
+            mPlaceValues(1) = "thousand"
+            mPlaceValues(2) = "million"
+            mPlaceValues(3) = "billion"
+            mPlaceValues(4) = "trillion"
+
+        End Sub
+
+
+        Private Function GetOnes(ByVal OneDigit As Integer) As String
+
+            GetOnes = ""
+
+            If OneDigit = 0 Then
+                Exit Function
+            End If
+
+            GetOnes = mOnesArray(OneDigit - 1)
+
+        End Function
+
+
+        Private Function GetTens(ByVal TensDigit As Integer) As String
+
+            GetTens = ""
+
+            If TensDigit = 0 Or TensDigit = 1 Then
+                Exit Function
+            End If
+
+            GetTens = mTensArray(TensDigit - 2)
+
+        End Function
+
+
+        Public Function ConvertNumberToWords(ByVal NumberValue As String) As String
+
+            Dim Delimiter As String = " "
+            Dim TensDelimiter As String = "-"
+            Dim mNumberValue As String = ""
+            Dim mNumbers As String = ""
+            Dim mNumWord As String = ""
+            Dim mFraction As String = ""
+            Dim mNumberStack() As String
+            Dim j As Integer = 0
+            Dim i As Integer = 0
+            Dim mOneTens As Boolean = False
+
+            ConvertNumberToWords = ""
+
+            ' validate input
+            Try
+                j = CDbl(NumberValue)
+            Catch ex As Exception
+                ConvertNumberToWords = "Invalid input."
+                Exit Function
+            End Try
+
+            ' get fractional part {if any}
+            If InStr(NumberValue, ".") = 0 Then
+                ' no fraction
+                mNumberValue = NumberValue
+            Else
+                mNumberValue = Microsoft.VisualBasic.Left(NumberValue, InStr(NumberValue, ".") - 1)
+                mFraction = Mid(NumberValue, InStr(NumberValue, ".")) ' + 1)
+                mFraction = Math.Round(CSng(mFraction), 2) * 100
+
+                If CInt(mFraction) = 0 Then
+                    mFraction = ""
+                Else
+                    mFraction = "&& " & mFraction & "/100"
+                End If
+            End If
+            mNumbers = mNumberValue.ToCharArray
+
+            ' move numbers to stack/array backwards
+            For j = mNumbers.Length - 1 To 0 Step -1
+                ReDim Preserve mNumberStack(i)
+
+                mNumberStack(i) = mNumbers(j)
+                i += 1
+            Next
+
+            For j = mNumbers.Length - 1 To 0 Step -1
+                Select Case j
+                    Case 0, 3, 6, 9, 12
+                        ' ones  value
+                        If Not mOneTens Then
+                            mNumWord &= GetOnes(Val(mNumberStack(j))) & Delimiter
+                        End If
+
+                        Select Case j
+                            Case 3
+                                ' thousands
+                                mNumWord &= mPlaceValues(1) & Delimiter
+
+                            Case 6
+                                ' millions
+                                mNumWord &= mPlaceValues(2) & Delimiter
+
+                            Case 9
+                                ' billions
+                                mNumWord &= mPlaceValues(3) & Delimiter
+
+                            Case 12
+                                ' trillions
+                                mNumWord &= mPlaceValues(4) & Delimiter
+                        End Select
+
+
+                    Case Is = 1, 4, 7, 10, 13
+                        ' tens value
+                        If Val(mNumberStack(j)) = 0 Then
+                            mNumWord &= GetOnes(Val(mNumberStack(j - 1))) & Delimiter
+                            mOneTens = True
+                            Exit Select
+                        End If
+
+                        If Val(mNumberStack(j)) = 1 Then
+                            mNumWord &= mOneTensArray(Val(mNumberStack(j - 1))) & Delimiter
+                            mOneTens = True
+                            Exit Select
+                        End If
+
+                        mNumWord &= GetTens(Val(mNumberStack(j)))
+
+                        ' this places the tensdelimiter; check for succeeding 0
+                        If Val(mNumberStack(j - 1)) <> 0 Then
+                            mNumWord &= TensDelimiter
+                        End If
+                        mOneTens = False
+
+                    Case Else
+                        ' hundreds value 
+                        mNumWord &= GetOnes(Val(mNumberStack(j))) & Delimiter
+
+                        If Val(mNumberStack(j)) <> 0 Then
+                            mNumWord &= mPlaceValues(0) & Delimiter
+                        End If
+                End Select
+            Next
+
+            Return mNumWord & mFraction
+
+        End Function
+
+
+
+
 End Class
 
 
